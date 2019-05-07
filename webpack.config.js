e7ebdf5aaabb113a1a18a517c7a69b7eb24e6f2c
Ted Shaffer <ted@brightsign.biz>
2019-05-07 13:48:08 -0700

    Add externals for BrightSign modules

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,21 @@ module.exports = {
     poll: true
   },
   target: 'electron',
+  externals: {
+    BSDeviceInfo : 'BSDeviceInfo',
+    '@brightsign/registry': 'commonjs @brightsign/registry',
+    '@brightsign/systemtime': 'commonjs @brightsign/systemtime',
+    '@brightsign/networkconfiguration': 'commonjs @brightsign/networkconfiguration',
+    '@brightsign/videooutput': 'commonjs @brightsign/videooutput',
+    '@brightsign/screenshot': 'commonjs @brightsign/screenshot',
+
+    '@brightsign/videomodeconfiguration': 'commonjs @brightsign/videomodeconfiguration',
+    '@brightsign/videoinput': 'commonjs @brightsign/videoinput',
+    '@brightsign/system': 'commonjs @brightsign/system',
+
+    '@brightsign/hostconfiguration': 'commonjs @brightsign/hostconfiguration',
+    '@brightsign/networkdiagnostics': 'commonjs @brightsign/networkdiagnostics',
+  },
   module: {
     loaders: [
       {
